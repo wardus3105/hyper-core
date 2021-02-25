bcbd3e19626fea9094bc0d44ef45b98a2744b322
Wardus <mobile-backend>
2021-02-25 11:24:39 +0700

    1

--- core.ts
+++ core.ts
@@ -1,2 +1,6 @@
-import { URL_PATHS } from 'networking/url-paths';
-export { URL_PATHS };
+import fetch from "node-fetch";
+const fetchtest = () => {
+    fetch('https://api.github.com/users/github').then(res => res.json);
+}
+export { fetchtest }
+
